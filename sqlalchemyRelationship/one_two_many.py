c43362c61150098ef44055f20fc020815cd0788d
LAPTOP-1LSV690M\上善 <595002753@qq.com>
2018-09-04 18:51:07 +0800

    新增迁移脚本

--- sqlalchemyRelationship/one_two_many.py
+++ sqlalchemyRelationship/one_two_many.py
@@ -1,4 +1,7 @@
+# coding: UTF-8
 from flask import Flask
+from flask_migrate import Migrate,  MigrateCommand
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,9 +9,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dba:111@45.78.9.17:3306
 db = SQLAlchemy(app)
 
 
+
+
+
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(20))
+    name = db.Column(db.String(20)),
+    age = db.Column(db.Integer),
+    hobby = db.Column(db.String(20)),
     pets = db.relationship("Pet", backref=db.backref('owner', lazy='dynamic'))
 
 
@@ -17,13 +25,22 @@ class Pet(db.Model):
     name = db.Column(db.String(20))
     owner_id = db.Column(db.Integer, db.ForeignKey("person.id"))
 
+
 # from one_two_many import db
 # db.create_all()
 
 
 # from one_to_may import Person,Pet
 
-# 如果我们在Person 中指定pets 的backref 为owner ，那么pet会自动添加一个属性owner  ,
+# 濡傛灉鎴戜滑鍦≒erson 涓寚瀹歱ets 鐨刡ackref 涓簅wner 锛岄偅涔坧et浼氳嚜鍔ㄦ坊鍔犱竴涓睘鎬wner  ,
 # spot = Pet(name='Spot',owner=anthony)
 # db.session.add(spot)
 # db.session.commit()
+
+Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+if '__main__' == __name__:
+    manager.run()
